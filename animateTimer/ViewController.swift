1f098687d7a6d8cd3ef5df35bd2bb9cea6e5f430
dit06 <dit@imac-5.local>
2019-03-27 00:31:45 -0700

    animation

--- animateTimer/ViewController.swift
+++ animateTimer/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var countLabel: UILabel!
+    var count=1;
+    var isAnimation=false;
+    var myTimer=Timer();
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        img.image=UIImage(named: "img1")
+        countLabel.text=String(count);
     }
 
-
+    @IBAction func playBut(_ sender: UIButton) {
+            //Timer start
+        myTimer=Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(playAnimation), userInfo: nil, repeats: true)
+    }
+    @objc func playAnimation(){
+        
+        if(count==5){
+            count=1;
+        }else{
+            count+=1;
+        }
+        
+        img.image=UIImage(named: "img\(count)");
+        countLabel.text=String(count);
+    }
+    @IBAction func pauseBut(_ sender: UIButton) {
+        myTimer.invalidate();
+    }
+    @IBAction func stopBut(_ sender: UIButton) {
+        myTimer.invalidate();
+        count=0;
+    }
 }
 
